8523bb58156061862a368ae2b94eab7ff8a4256e
Aqil Gifari <aqilgifari0@gmail.com>
2022-03-23 14:21:58 +0700

    revert loading

--- src/pages/store/store.js
+++ src/pages/store/store.js
@@ -48,7 +48,6 @@ function Store() {
     <>
       <WrapperScene assets={<Assets />}>
         <a-entity gltf-model="#store" position="0 0 0"></a-entity>
-        <a-scene loading-screen="dotsColor: red; backgroundColor: white"></a-scene>
         {/* <Entity
           primitive="a-circle"
           events={{
