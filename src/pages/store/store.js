2cf162960a865e8f1e41bc68e075522ad3c5d2e7
Aqil Gifari <aqilgifari0@gmail.com>
2022-03-24 18:04:34 +0700

    add camera cursor

--- src/pages/store/store.js
+++ src/pages/store/store.js
@@ -6,10 +6,7 @@ import DrinkMenuContent from "./components/drinkMenuContent";
 const Assets = () => {
   return (
     <a-assets>
-      <a-asset-item
-        id="store"
-        src="/assets/alfamart-6.glb"
-      ></a-asset-item>
+      <a-asset-item id="store" src="/assets/alfamart-6.glb"></a-asset-item>
     </a-assets>
   );
 };
@@ -45,9 +42,11 @@ function Store() {
   const urlSearchParams = new URLSearchParams(window.location.search);
   const params = Object.fromEntries(urlSearchParams.entries());
 
+  const forceVR = params?.["vr-mode"] !== undefined;
+  const noVR = params?.["plain"] !== undefined;
   return (
     <>
-      <WrapperScene assets={<Assets />} noVr={params?.["plain"] !== undefined} forceVR={params?.["vr-mode"] !== undefined}>
+      <WrapperScene assets={<Assets />} noVr={noVR} forceVR={forceVR}>
         <a-entity gltf-model="#store" position="0 0 0"></a-entity>
         {/* <Entity
           primitive="a-circle"
@@ -123,18 +122,34 @@ function Store() {
         <a-entity light="color: #fff; intensity: 1" position="-4 4 -5"></a-entity>
         <a-entity light="color: #fff; intensity: 0.2" position="4 -4 5"></a-entity>
         <a-entity light="color: #fff; intensity: 0.2" position="-4 -4 -5"></a-entity> */}
-        <a-camera
-          position="0 1.5 0"
-          // wasd-controls-enabled="false"
-          touch-enabled="true"
-          id="camera"
-        >
-          <a-entity
-            cursor="rayOrigin: mouse; fuseTimeout: 0"
-            position="0 0 -1"
-            geometry="primitive: ring; radiusOuter: 0; radiusInner: 0"
-          ></a-entity>
-        </a-camera>
+        {forceVR ? (
+          <a-camera
+            position="0 1.5 0"
+            touch-enabled="true"
+            id="camera"
+            camera
+            look-controls
+          >
+            <a-entity
+              cursor="fuse: true; fuseTimeout: 500"
+              position="0 0 -1"
+              geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
+              material="color: black; shader: flat"
+            ></a-entity>
+          </a-camera>
+        ) : (
+          <a-camera
+            position="0 1.5 0"
+            touch-enabled="true"
+            id="camera"
+          >
+            <a-entity
+              cursor="rayOrigin: mouse; fuseTimeout: 0"
+              position="0 0 -1"
+              geometry="primitive: ring; radiusOuter: 0; radiusInner: 0"
+            ></a-entity>
+          </a-camera>
+        )}
         <DrinkMenu
           open={menuOpen?.status}
           setOpen={setOpenMenu}
