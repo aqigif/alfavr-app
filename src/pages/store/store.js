cf7a20266a9f641713d3c94ce103d81dae1aa17a
Aqil Gifari <aqilgifari0@gmail.com>
2022-03-25 13:44:23 +0700

    fusing scale up animation

--- src/pages/store/store.js
+++ src/pages/store/store.js
@@ -227,7 +227,7 @@ function Store() {
               <a-cursor
                 color="red"
                 position="0 0 -1"
-                animation__fusing="property: scale; startEvents: fusing; easing: easeInCubic; dur: 1500; from: 1 1 1; to: 1.3 1.3 1.3"
+                animation__fusing="property: scale; startEvents: fusing; easing: easeInCubic; dur: 1500; from: 1 1 1; to: 2 2 2"
               ></a-cursor>
             </a-camera>
           </a-entity>
