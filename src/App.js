ffd4755d312a06a42520d5b7b201a18654966cf4
Aqil Gifari <aqilgifari0@gmail.com>
2022-04-08 17:22:49 +0700

    add mili

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import "aframe";
 // import "aframe-button-controls";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Store from "./pages/store/store";
+import Alfa from "./pages/alfa";
+import Mili from "./pages/mili";
 
 
 function App() {
-  return <Store />;
+  return (
+    <Routes>
+      <Route path="/" element={<Alfa />} />
+      <Route path="mili" element={<Mili />} />
+    </Routes>)
 }
 
 export default App;
